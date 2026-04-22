781ba5fa4726a5204f6ce4e56cf6a33d78a7a8f8
Mehdi Morshed Chowdhury <mmc1641992@gmail.com>
2026-04-22 10:14:03 +0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,14 +6,14 @@ import sys
 import os
 
 # ── Update this path with YOUR PythonAnywhere username ─────────────────────
-PROJECT_HOME = '/home/YOURUSERNAME/geopolitical-forecaster'
+PROJECT_HOME = '/home/datacodex/geopolitical-forecaster'
 # ───────────────────────────────────────────────────────────────────────────
 
 if PROJECT_HOME not in sys.path:
     sys.path.insert(0, PROJECT_HOME)
 
 # Set environment variables here if not using .env file
-os.environ['NEWS_API_KEY']       = 'YOUR_NEWSAPI_KEY_HERE'
-os.environ['OPENROUTER_API_KEY'] = 'YOUR_OPENROUTER_KEY_HERE'
+os.environ['NEWS_API_KEY']       = 'cd24517469524b30acdccd9f3946c76f'
+os.environ['OPENROUTER_API_KEY'] = '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'
 
 from app import application  # noqa: F401
